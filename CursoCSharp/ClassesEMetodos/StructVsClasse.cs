338a8e6cd47312ce9df23ca8bee5e74d3be35fed
Cairo Braz Costa <cairo.costa@animaeducacao.com.br>
2023-07-27 19:51:04 -0300

    Aula 62: Atribuicao por valor (struct) x atibuicao por referencia(class)

--- CursoCSharp/ClassesEMetodos/StructVsClasse.cs
+++ CursoCSharp/ClassesEMetodos/StructVsClasse.cs
@@ -19,12 +19,14 @@ namespace CursoCSharp.ClassesEMetodos
         public static void Executar()
         {
             SPonto ponto1 = new SPonto { X = 1, Y = 3 };
-            SPonto copiaPonto1 = ponto1; // Atribuição por VALOR!!
+            SPonto copiaPonto1 = ponto1; // STRUCT -> NÃO APONTAM PARA O MESMO LUGAR NA MEMÓRIA. PORTANTO É UMA ATRIBUIÇÃO POR VALOR
+                                         // CHAMADO DE CÓPIA POR VALOR, POIS A CÓPIA OCUPA UM NOVO ESPAÇO NA MEMÓRIA.
 
             ponto1.X = 5;
 
             Console.WriteLine("Ponto 1. X: {0}", ponto1.X);
-            Console.WriteLine($"Cópia ponto 1. X:{copiaPonto1.X}");
+            Console.WriteLine($"Cópia ponto 1. X:{copiaPonto1.X}");// STRUCT -> NÃO APONTAM PARA O MESMO LUGAR NA MEMÓRIA. PORTANTO É UMA ATRIBUIÇÃO POR VALOR
+                                                                   // CHAMADO DE CÓPIA POR VALOR, POIS A CÓPIA OCUPA UM NOVO ESPAÇO NA MEMÓRIA.
 
             CPonto ponto2 = new CPonto { X = 10, Y = 40 };
             CPonto copiaDoPonto2 = ponto2; // Atribuição por referência
@@ -32,6 +34,7 @@ namespace CursoCSharp.ClassesEMetodos
             ponto2.X = 55;
             Console.WriteLine($"Ponto 2. X: {ponto2.X}");
             Console.WriteLine($"Cópia do ponto 2. X: {copiaDoPonto2.X}");
+            // CLASS -> APONTAM PARA O MESMO LUGAR NA MEMÓRIA. PORTANTO É UMA CÓPIA POR REFERÊNCIA. SE ALTERAR Ó ORIGINAL, TAMBÉM ALTERA A CÓPIA.
 
 
         }
